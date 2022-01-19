6e39295d331a13c23cb5ec4e053795f49ddbaf3f
P00ko <nohjoed@protonmail.com>
2022-01-19 15:28:47 +0000

    Second Function Ex.

--- hello.js
+++ hello.js
@@ -2,6 +2,14 @@ const sayHello = function(name) {
   console.log("Hello, " +name );
 }
 
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
